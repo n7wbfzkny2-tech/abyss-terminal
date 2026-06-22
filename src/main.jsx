41169bf9598d21f3f0994b70ad8a747d298fd69f
n7wbfzkny2-tech <n7wbfzkny2@privaterelay.appleid.com>
2026-06-23 02:58:04 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CryptoTerminal from "./CryptoTerminal.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CryptoTerminal />
+  </React.StrictMode>
+);
